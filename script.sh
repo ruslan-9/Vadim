3d5448164e4aae9826996b3d476c11bd1a45110c
Ruslan <56490049+ruslan-9@users.noreply.github.com>
2023-04-16 23:58:32 +0300

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd homework
+mkdir folder_1 folder_2 folder_3
+cd folder_1
+touch 1.txt 2.txt 3.txt 1.json 2.json
+mkdir Saint-Petersburg Moscow Novorossiysk
+ls -la
+mv 1.txt 1.json ../folder_2/
